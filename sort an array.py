fac732bdf2dad3d35e635bb28114a8a6bf2fd842
mahvish23 <70450130+mahvish23@users.noreply.github.com>
2020-10-28 19:51:30 +0530

    Update sort an array.py

--- sort an array.py
+++ sort an array.py
@@ -1,8 +1,13 @@
-"""SORT AN ARRAY USING RECURSION"""
+"""SORT AN ARRAY USING RECURSION 
+input: a=[1,2,3]
+s=[3,2,1]
+output:
+s=[3,2,1]
+"""
 
 
 def insertarr(arr, ele):
-    if len(arr) == 0 or ele <= arr[-1] :
+    if len(arr) == 0 or ele >= arr[-1] :
         arr.append(ele)
         return
     val = arr[-1]
