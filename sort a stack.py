d34171dc1ce0d198634ed8171bb3cde73f88ab43
mahvish23 <70450130+mahvish23@users.noreply.github.com>
2020-10-28 19:47:27 +0530

    Add files via upload

---
+++ sort a stack.py
@@ -0,0 +1,18 @@
+"""" SORT A STACK """
+def insertstack(arr,ele):
+    if len(arr)==0 or arr[-1]>=ele:
+        arr.append(ele)
+        return
+        val=arr.pop()
+        insertstack(arr,ele)
+        arrr.append(val)
+def sortstack(arr):
+    if len(arr)==1:
+        return
+    ele=arr.pop()
+    sortstack(arr)
+    insertstack(arr,ele)
+a=list(map(int,input().split()))
+sortstack(a)
+for i in a:
+    print(i,end=" ")
